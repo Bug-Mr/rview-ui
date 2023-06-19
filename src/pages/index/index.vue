03302b83a0a36eccb18f9c0de8bcddac709c1989
杨梦桃 <1920872385@qq.com>
2023-06-19 16:12:05 +0800

    【更改】

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <view>
-    <r-icon type="arrow-left"></r-icon>
+  <view class="page-box">
+    <r-section title="Button" subTitle="按钮" @click="gotopath('button')" rightIcon type="line"></r-section>
+    <r-section title="Icon" subTitle="Icon图标" @click="gotopath('icon')" rightIcon type="line"></r-section>
+    <r-section title="Section" subTitle="标题栏" @click="gotopath('section')" rightIcon type="line"></r-section>
+    <r-section title="Layout" subTitle="栅格布局" @click="gotopath('layout')" rightIcon type="line"></r-section>
   </view>
 </template>
 
 <script lang="ts" setup>
-import { $r } from "../../components/index";
-console.log($r.version)
+function gotopath(key: string) {
+  uni.navigateTo(
+    {
+      url: `/pages/r-${key}/index`
+    }
+  )
+}
 </script>
+
+<style lang="scss" scoped>
+.page-box {
+  padding: 20rpx;
+  height: calc(100vh - 150rpx);
+  background: #f5f5f5;
+}
+</style>
